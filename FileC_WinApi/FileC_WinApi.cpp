bef7d2201e6ee2bcb76f013090b83eaf7c76068b
Кирилл Манаков <kirill_manakowww2@mail.ru>
2022-10-29 17:09:28 +0300

    Добавьте файлы проекта.

---
+++ FileC_WinApi/FileC_WinApi.cpp
@@ -0,0 +1,28 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+
+int WINAPI WinMain(HINSTANCE hlnstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	int message = MessageBox(NULL, L"Îøèáêà", L"APP", MB_YESNOCANCEL | MB_ICONQUESTION);
+
+	switch (message)
+	{
+	case IDYES:
+	{
+		MessageBox(NULL, L"Âû íàæàëè íà ÄÀ", L"APP", NULL);
+		break;
+	}
+	case IDNO:
+	{
+		MessageBox(NULL, L"Âû íàæàëè íà ÍÅÒ", L"APP", NULL);
+		break;
+	}
+	case IDCANCEL:
+	{
+		MessageBox(NULL, L"Ïðîãðàììà çàêðûòà", L"APP", NULL);
+		break;
+	}
+
+	}
+	return 0;
+}
